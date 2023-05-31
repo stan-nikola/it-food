0f9c049a379f096a945a4fbcc3bd0d5c1c4bfadb
Sergey J <aleks.s.educ@gmail.com>
2023-05-31 20:28:15 +0300

    Add results for May, 31

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,16 +1,77 @@
+// import { NavLink } from 'react-router-dom';
+
 import css from './LeftSideBar.module.css';
+// import Bread from '../../images/svg/Appetizer.svg';
+import { ReactComponent as Bread } from '../../images/svg/Appetizer.svg';
+
+// import e from '../../images/svg/';
 
 export const LeftSideBar = () => {
   return (
     <section className={css.container}>
-      LeftSideBar
-      {/* <div className={css.wraper}>
-        <FoodTypeButton name="All" />
+      <ul className={css.wrapper}>
+        <li className={css.listItem}>
+          {/* <NavLink
+          // className={css.link}
+          // to={}
+          // state={}
+          >
+            <img
+              src="../../images/svg/Coffee.svg"
+              alt="Icon"
+              width="77"
+              height="78"
+            />
+          </NavLink> */}
+        </li>
+        <li className={css.listItem}>
+          <Bread />
+          {/* <img
+            src="../../images/svg/Beverages.svg"
+            alt="Icon"
+            width="77"
+            height="78"
+          /> */}
+        </li>
+        <li className={css.listItem}>
+          <img
+            src="../../images/svg/MainCourse.svg"
+            alt="Icon"
+            width="77"
+            height="78"
+          />
+        </li>
+        <li className={css.listItem}>
+          <img
+            src="../../images/svg/Appetizer.svg"
+            alt="Icon"
+            width="77"
+            height="78"
+          />
+        </li>
+        <li className={css.listItem}>
+          <img
+            src="../../images/svg/Bread.svg"
+            alt="Icon"
+            width="77"
+            height="78"
+          />
+        </li>
+        <li className={css.listItem}>
+          <img
+            src="../../images/svg/Snack.svg"
+            alt="Icon"
+            width="77"
+            height="78"
+          />
+        </li>
+
+        {/* <FoodTypeButton name="All" />
         <FoodTypeButton name="Chicken" />
         <FoodTypeButton name="Seafood" />
         <FoodTypeButton name="Pasta" />
-        <FoodTypeButton name="Rice bowl" />
-      </div> */}
+        <FoodTypeButton name="Rice bowl" /> */}
+      </ul>
     </section>
   );
 };
