b51e51839550b00a406904971d0883da74e84ca3
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-06-03 23:25:48 +0300

    add auth sign in redux state

---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import { authPresistedReducer } from './auth/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authPresistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
