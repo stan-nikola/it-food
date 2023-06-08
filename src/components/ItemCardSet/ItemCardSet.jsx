be5c55d2640f10687a8c7384bc02236ef18e3316
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-06-08 19:50:43 +0300

    fix fetch

--- src/components/ItemCardSet/ItemCardSet.jsx
+++ src/components/ItemCardSet/ItemCardSet.jsx
@@ -3,30 +3,19 @@ import { ItemCard } from '../ItemCard/ItemCard';
 import css from './ItemCardSet.module.css';
 import axios from 'axios';
 
-import { arrayOfCards } from '../../../src/recipes-17';
-
 axios.defaults.baseURL = 'http://localhost:3000/api/';
 
-export const ItemCardSet = async () => {
-  // const { } = useState()
+export const ItemCardSet = () => {
+  const [arrayOfCards, setArrayOfCards] = useState([]);
 
-  // useEffect(() => {
-  //   const res = fetchAllItems();
-  //   console.log('res=', res);
-  // }, []);
-
-  // const arrayOfCards = JSON.parse(res);
-  const arrayOfCards = await fetchAllItems();
-  console.log('arrayOfCards=', arrayOfCards);
-  // const q = []
-  // console.log('arrayOfCards=', arrayOfCards);
-  // console.log(typeof arrayOfCards);
+  useEffect(() => {
+    fetchAllItems().then(data => setArrayOfCards(data));
+  }, []);
 
   return (
     <>
       <ul className={css.itemCardSet}>
         {arrayOfCards.map(item => {
-          console.log(item);
           const { _id, preview, title, price } = item;
           return (
             <li key={_id}>
@@ -43,10 +32,6 @@ export const ItemCardSet = async () => {
 const fetchAllItems = async () => {
   try {
     const response = await axios.get('/items');
-    // const x = response.data;
-    // console.log('DATA=', x);
-    // console.log(typeof x);
-    // console.log('response=', response);
     return response.data;
   } catch (error) {
     console.log(error);
