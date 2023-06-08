00a502c9569c314a1abd214bc423fbbaf833cd99
Sergey J <aleks.s.educ@gmail.com>
2023-06-08 19:11:06 +0300

    ArrayOfCards

---
+++ src/components/ItemCardSet/111.js
@@ -0,0 +1,20 @@
+{
+  /* <ul className={css.itemCardSet}> */
+}
+{
+  /* {arrayOfCards.map(item => {
+          console.log(item);
+          const { _id, preview, title, price } = item;
+          return (
+            <li key={_id}>
+              <ItemCard imgUrl={preview} name={title} price={price} />
+            </li>
+          );
+        })} */
+}
+{
+  /* <ItemCard /> */
+}
+{
+  /* </ul> */
+}
--- src/components/ItemCardSet/ItemCardSet.jsx
+++ src/components/ItemCardSet/ItemCardSet.jsx
@@ -1,23 +1,54 @@
+import { useEffect, useState } from 'react';
 import { ItemCard } from '../ItemCard/ItemCard';
 import css from './ItemCardSet.module.css';
+import axios from 'axios';
+
 import { arrayOfCards } from '../../../src/recipes-17';
 
-// console.log(arrayOfCards);
+axios.defaults.baseURL = 'http://localhost:3000/api/';
 
-export const ItemCardSet = () => {
+export const ItemCardSet = async () => {
+  // const { } = useState()
+
+  // useEffect(() => {
+  //   const res = fetchAllItems();
+  //   console.log('res=', res);
+  // }, []);
+
+  // const arrayOfCards = JSON.parse(res);
+  const arrayOfCards = await fetchAllItems();
+  console.log('arrayOfCards=', arrayOfCards);
+  // const q = []
+  // console.log('arrayOfCards=', arrayOfCards);
+  // console.log(typeof arrayOfCards);
+
   return (
     <>
       <ul className={css.itemCardSet}>
         {arrayOfCards.map(item => {
-          // console.log('item.title====', item.title);
+          console.log(item);
+          const { _id, preview, title, price } = item;
           return (
-            <li key={item._id.$oid}>
-              <ItemCard imgUrl={item.preview} name={item.title} price="50" />
+            <li key={_id}>
+              <ItemCard imgUrl={preview} name={title} price={price} />
             </li>
           );
         })}
-        {/* <ItemCard /> */}
+        <ItemCard />
       </ul>
     </>
   );
+};
+
+const fetchAllItems = async () => {
+  try {
+    const response = await axios.get('/items');
+    // const x = response.data;
+    // console.log('DATA=', x);
+    // console.log(typeof x);
+    // console.log('response=', response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
