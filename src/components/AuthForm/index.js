b5bab6e4603ad29d1e1a101018e8bee4bc361a15
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-07-27 22:20:04 +0300

    fix reexport

--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -1,5 +1,5 @@
 import { LogInForm } from './LogInForm';
 import { SignUpForm } from './SignUpForm';
-import { LogOutForm } from './LogOutForm';
+import { LogOutForm } from './LogOutForm1';
 
 export { LogInForm, SignUpForm, LogOutForm };
