ee9884276aaec2c827e24b5717eedfef3cc44942
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-07-28 18:36:11 +0300

    fix errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
